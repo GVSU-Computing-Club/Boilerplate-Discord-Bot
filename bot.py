408f9e3eac5ca0ef091c6a9e7871353ed284ee3c
Eric Darr <darre@mail.gvsu.edu>
2018-09-27 17:42:09 -0400

    added challenges

--- bot.py
+++ bot.py
@@ -32,8 +32,57 @@ class Bot(discord.Client):
     async def on_typing(self, channel, user, when):
         print('User: [{}] typing at time: {}:{}:{}'.format(user.display_name, self.fixHour(when.hour), when.minute, when.second))
 
+    ########################
+    #challenges            #
+    ########################
+
+    # 20 point challenges:
+
+    #reverse incoming string message and print it back to the user
+    #example:
+    #!reverse hello world
+    #output:
+    #dlrow olleh
+    def reverse(self, message, channel):
+        pass
+
+
+    # 50 point challenges:
+
+    # list all users who are online currently
+    def list_online(self, channel):
+        pass
+
+    # 100 point challenges
+
+    #rock, paper, scissors
+    #When you !rps the bot will countdown from 5 and randomly choose rock, paper, or scissors
+    #if the user responds
+    def rps(self, message, channel):
+        pass
+
+    #get the weather for a specific location
+    def weather(self, message, channel):
+        pass
+
+    #get the time in a certain location of the world and print to the user
+    def time_in(self, message, channel):
+        pass
+
+    # 300 points
+    # count individual words of any message and store them into a file as a dictionary
+    # hint use json, and the json.dump to store it as a dictionary in a file
+    def word_count(self, message):
+        pass
+
+    # part 2 of previous challenges:
+    # 200 points extra
+    # using !topwords get the top 5 most commonly used words and their count
+    # hint: use json.load to read the information from the file as a dictionary
+    def top_words(self, message, channel):
+        pass
+
     async def on_message(self, message):
         if(message.content.startswith('hello')):
             await self.send_message(message.channel, 'world')
-
 
